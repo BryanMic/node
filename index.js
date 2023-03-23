1424d4f885e87992ec004fe0e7784248173e7b60
BryanMic <bryan.michel.via@gmail.com>
2023-03-23 16:29:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-// const express = require('express')
-// const app = express()
-
-// const parkings = require('./parkings.json')
-// // app.get('/parkings', (req, res) => {res.send("Parkings : ")} )
-// app.get('/parkings', (req, res) => { res.status(200).json(parkings)} )
-
-// app.listen(8080, () => { console.log("server go") })
-
-
 const express = require('express')
 const app = express()
-const parkings = require('./parkings.json')
 const user = require('./user.json')
 
 app.use(express.json())
-
-app.get('/parkings', (req, res) => {
-    res.status(200).json(parkings)
-})
-
-app.get('/parkings/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const parking = parking.find(parking => parking.id === id)
-    res.status(200).json(parking)
-})
-
-app.post('/parkings', (req, res) => {
-    parkings.push(req.body)
-    res.status(200).json(parkings)
-})
 
 app.get('/user', (req, res) => {
     res.status(200).json(user)
@@ -40,15 +14,27 @@ app.get('/user/:id', (req, res) => {
     res.status(200).json(user)
 })
 
-app.post('/user', (req, res) => {
-    parkings.push(req.body)
-    res.status(200).json(parkings)
+app.delete('/user/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = user.find(user => user.id === id)
+    user.splice(user.indexOf(user),1)
+    res.status(200).json(user)
 })
 
+app.put('user/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    let user = user.find(user => user.id === id)
+    user.user = req.body.user,
+    user.mdp = req.body.mdp,
+    res.status(200).json(user)
+})
+
+app.post('/user', (req, res) => {
+    user.push(req.body)
+    res.status(200).json(user)
+})
+ 
 
 app.listen(8080, () => { 
     console.log("server go") 
 })
-
-
-
